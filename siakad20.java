4337dfaa5e83919306a282fe21d48983fb973951
rojalingga <rojalingga2006@gmail.com>
2024-09-27 09:24:07 +0700

    siakad revisi

--- siakad20.java
+++ siakad20.java
@@ -5,17 +5,16 @@ public class siakad20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String nama, nim;
-        char kelas;
+        String nama, nim, kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUTS, nilaiAkhir, nilaiUAS;
 
         System.out.print("Masukkan nama : ");
         nama = sc.nextLine();
         System.out.print("Masukkan NIM : ");
         nim = sc.nextLine();
         System.out.print("Masukkan Kelas : ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.print("Masukkan nomor absen : ");
         absen = sc.nextByte();
 
@@ -25,13 +24,15 @@ public class siakad20 {
         nilaiKuis = sc.nextDouble();
         System.out.print("Masukkan nilai tugas : ");
         nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai ujian : ");
-        nilaiUjian = sc.nextDouble();
+        System.out.print("Masukkan nilai UTS : ");
+        nilaiUTS = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS : ");
+        nilaiUAS = sc.nextDouble();
 
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        nilaiAkhir = ((nilaiKuis*20/100)+(nilaiTugas*15/100)+(nilaiUTS*30/100)+(nilaiUAS*35/100));
 
         System.out.print("Mahasiswa dengan nama "+nama+" (NIM "+nim+") ");
-        System.out.println("kelas : " +kelas+ " Absen: " +absen);
+        System.out.println("kelas : "+kelas+" nomor absen: "+absen);
         System.out.println("Nilai Akhir : "+nilaiAkhir);
         
     }
